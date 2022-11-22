bda43e4b86ff871176e66e0e455db83eaa380de4
Ruslan Tendora <ruslantendora@gmail.com>
2022-11-22 11:31:10 +0200

    add 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(saveTimeVideo, 1000));
+
+function saveTimeVideo(data) {
+  console.log(data.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
