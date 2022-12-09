9869f7735b59fdb62f4a113c9778afaec9fb92c9
Ruslan Tendora <ruslantendora@gmail.com>
2022-12-09 15:45:44 +0200

    add handlebars

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,9 +3,8 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 import { galleryItemsEl } from './create-gallery';
+import cardMakup from '../templates/card.hbs';
 // Change code below this line
-
-console.log(galleryItems);
 
 const galleryBoxEl = document.querySelector('.gallery');
 
@@ -15,3 +14,7 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+
+// const markupCards = galleryItems.map(card => cardMakup(card)).join('');
+
+// console.log(markupCards);
